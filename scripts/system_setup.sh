950b3f4b43b23b1d868cc674de8378cf0ba40c6c
Ege Çetin <egecetin@hotmail.com.tr>
2023-12-25 20:55:35 +0300

    remove quiet mode

--- scripts/system_setup.sh
+++ scripts/system_setup.sh
@@ -10,7 +10,7 @@ sudo apt-get update && apt-get install -y \
 python3 -m pip install --upgrade pip
 python3 -m pip install -r ../data/requirements.txt
 
-wget -q -O esa-snap_all_unix_9_0.sh "http://step.esa.int/downloads/9.0/installers/esa-snap_all_unix_9_0_0.sh"
+wget -O esa-snap_all_unix_9_0.sh "http://step.esa.int/downloads/9.0/installers/esa-snap_all_unix_9_0_0.sh"
 bash esa-snap_all_unix_9_0.sh -q -varfile ../data/response.varfile
 rm esa-snap_all_unix_9_0.sh
 
